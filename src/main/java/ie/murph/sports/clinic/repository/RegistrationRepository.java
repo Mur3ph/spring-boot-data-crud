78561612d89980f03047a45710f2612dadbbe69c
Mur3ph <paulmurphymainstreet@gmail.com>
2016-10-07 23:24:50 +0100

    Registration repo

---
+++ src/main/java/ie/murph/sports/clinic/repository/RegistrationRepository.java
@@ -0,0 +1,9 @@
+package ie.murph.sports.clinic.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ie.murph.sports.clinic.domain.Person;
+
+public interface RegistrationRepository extends CrudRepository<Person, Long> {
+
+}
