036973f8780c7417e8402db61ba1d1a82e61a38f
Mur3ph <paulmurphymainstreet@gmail.com>
2016-10-08 23:32:55 +0100

    repo query

--- src/main/java/ie/murph/sports/clinic/repository/RegistrationRepository.java
+++ src/main/java/ie/murph/sports/clinic/repository/RegistrationRepository.java
@@ -1,9 +1,16 @@
 package ie.murph.sports.clinic.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import ie.murph.sports.clinic.domain.Person;
 
 public interface RegistrationRepository extends CrudRepository<Person, Long> {
 
+		// In the HQL , you should use the java class name and property name 
+		// of the mapped @Entity instead of the actual table name and column 
+		// name , so the HQL should be :
+		// @Query("SELECT p FROM Person p WHERE p.username = :username AND p.password = :password")
+		Person registerNewUser(@Param("username") String username, @Param("password")String password);
 }
